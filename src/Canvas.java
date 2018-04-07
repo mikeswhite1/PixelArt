5b2c765dad07221ad9a30d179879463e029c2d72
Mike White <mike.s.white1@gmail.com>
2018-04-07 14:37:01 -0500

    Canvas

---
+++ src/Canvas.java
@@ -0,0 +1,42 @@
+import javax.imageio.ImageIO;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Canvas {
+	Canvas(Stage primaryStage, GridPane gPane) {
+
+	    gPane.setPrefSize(600, 600);
+	    gPane.setGridLinesVisible(true);
+
+		Button[][] TileArray = new Button[5][5]; 
+		
+		for (int i = 0; i < 5; i++) {
+			 ColumnConstraints column = new ColumnConstraints(120);
+	         gPane.getColumnConstraints().add(column);
+			 RowConstraints row = new RowConstraints(120);
+	         gPane.getRowConstraints().add(row);
+			for (int ii = 0; ii < 5; ii++) {
+				Button tile = new Button();
+				tile.setStyle("-fx-border-color: #FFFFFF; -fx-border-width: .25px; -fx-font: 22 arial; -fx-base: #000000;");
+				tile.setMaxWidth(Double.MAX_VALUE);
+				tile.setMaxHeight(Double.MAX_VALUE);
+				tile.setText("" + (i*5 + ii + 1));
+				TileArray[i][ii] = tile;
+				gPane.add(tile, ii, i);
+			}
+		}
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,52 @@
+import java.io.File;
+import java.util.Scanner;
 
-public class Main {
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class Main extends Application  {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  JFileChooser chooser = new JFileChooser();
+		    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "JPG & GIF Images", "jpg", "gif");
+		    chooser.setFileFilter(filter);
+		    int returnVal = chooser.showOpenDialog(chooser);
+		    if(returnVal == JFileChooser.APPROVE_OPTION) {
+		       System.out.println("You chose to open this file: " +
+		            chooser.getSelectedFile().getName());
+		    }
+//		String fileName = "file.txt";
+//		File inputFile = null;
+//		Scanner sc = null;
+//
+//		try {
+//			inputFile = new File(fileName);
+//			sc = new Scanner(inputFile);
+//			sc.close();
+//		} catch (Exception ex) {
+//			ex.printStackTrace();
+//			System.exit(-1);
+//		}
 
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Form");
+		GridPane gPane = new GridPane();
+		Scene scene = new Scene(gPane, 600, 600, Color.DARKGRAY);
+		new Canvas(primaryStage, gPane);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 }
